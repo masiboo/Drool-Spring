802d106f50415f66959a9151755d515b2da0eac5
Masum Islam <madsum@gmail.com>
2021-03-22 12:55:21 +0200

    Update for xls files drools rules

--- src/main/java/com/javatechie/spring/drools/api/MegaOfferController.java
+++ src/main/java/com/javatechie/spring/drools/api/MegaOfferController.java
@@ -2,6 +2,7 @@ package com.javatechie.spring.drools.api;
 
 import org.kie.api.runtime.KieSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
